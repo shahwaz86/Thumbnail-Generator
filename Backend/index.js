824ba7e827d4e731d11c17b1faee41582d85bab8
Shahwaz Alam <shahwaz575@gmail.com>
2026-02-23 08:46:31 +0530

    index file changed

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "https://thumbnail-generator-eta.vercel.app",
+  origin: "https://thumbnail-generator-kappa.vercel.app",
   credentials: true,
 }));
 
